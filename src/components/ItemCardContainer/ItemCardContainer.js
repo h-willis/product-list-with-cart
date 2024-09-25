3055b88b0f5fecb47ad1a239ba2a5bb554a73b10
h-willis <harry.willis@btconnect.com>
2024-09-25 19:38:33 +1200

    messing with adaptive width

--- src/components/ItemCardContainer/ItemCardContainer.js
+++ src/components/ItemCardContainer/ItemCardContainer.js
@@ -12,7 +12,13 @@ function ItemCardContainer({ product }) {
 
   return (
     <div className={`itemCard`}>
-      <img src={product.image.desktop} alt={product.name} className={`${selected ? 'selected' : ''}`} />
+      {/* <img src={product.image.desktop} alt={product.name} className={`${selected ? 'selected' : ''}`} /> */}
+      <img className={`${selected ? 'selected' : ''}`}
+        srcset={`${product.image.mobile} 654w,
+        ${product.image.tablet} 427w,
+        ${product.image.desktop} 502w`}
+        sizes={"(max-width: 1124px) 502px, (max-width: 760px) 427px, (max-width: 375px) 654px"}
+        alt={product.name} />
       <AddToCart item={product.name} price={product.price} onSelection={handleSelection} />
       <p className='category'>{product.category}</p>
       <h3 className='name'>{product.name}</h3>
