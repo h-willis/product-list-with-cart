adb9945217477c64d3abfffcfe1cb86e8d7bca4c
h-willis <harry.willis@btconnect.com>
2024-09-27 15:07:25 +1200

    move css import in add to cart

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useCart } from '../../providers/CartProvider';
-import './AddToCart.css';
 import AddToCartInactive from './AddToCartInactive/AddToCartInactive';
 import AddToCartActive from './AddToCartActive/AddToCartActive';
+import './AddToCart.css';
 
 function AddToCart({ item, price, onSelection }) {
   const [amount, setAmount] = useState(0);
